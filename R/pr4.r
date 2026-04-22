812190ad30ee93762b2242bb0ebd42f968421ed3
AvengerVishwa358 <vishwajitkhade447@gmail.com>
2026-04-22 10:18:22 +0530

    Create pr4.r

---
+++ R/pr4.r
@@ -0,0 +1,14 @@
+Aim : Explore R program that include different operators
+
+w<-12; a<-2; r<-4 # Leftward assignment
+qt <- (w+a)/(w-r)
+print(qt)
+
+r1<-14; r2<-32;
+print(r1<r2)
+43 -> x #Rightward assignment
+print(x)
+
+dc <- c(10,42,32,22);
+check_in <- 32 %in% dc
+print(check_in)
