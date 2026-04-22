12d60b892412ade5ee43d8a8ea7d725d9643285b
AvengerVishwa358 <vishwajitkhade447@gmail.com>
2026-04-22 09:55:29 +0530

    Create pr3.r

---
+++ R/pr3.r
@@ -0,0 +1,15 @@
+Aim : Implement a R program that includes variable, constants.
+
+v1 <- 12.22
+print(v1)
+
+a <- b <- c <- "Unicorn"
+a
+b
+c
+
+height <- 12
+radius <- 5
+
+area_of_cylinder <- pi * radius * radius
+print(area_of_cylinder)
